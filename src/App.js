90e2eeb7bd78ac073081dda59ef68708260661ae
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-29 20:50:41 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
 import Form from './Form';
+
+
 
 function App() {
   return (
     <div className="App">
-     <p>Hi I'm paul, from the Jimmy Neutron</p>
+      <h1>Testing Inputs</h1>
      <Form/>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,36 +1,65 @@
 import { useState } from 'react';
 
 function Form (props) {
-    const [inputValue, setInputValue] = useState('')
+    
+    const initialValues = {
+        name: '',
+        email: '',
+        role: ''
+      }
 
+    const [formValues, setFormValues] = useState(initialValues)
+
+    function updateButtons (inputName, inputValue) {
+
+        setFormValues({...formValues, [inputName]: inputValue})
+    }
+
+    
     function typing (event) {
-        setInputValue(event.target.value)
+        const name = event.target.name;
+        const value = event.target.value;
+
+        updateButtons(name,value)
     }
 
     function submitting (event) {
         event.preventDefault()
-        inputValue.replace(/\s+/g, ' ').trim()
-        console.log(inputValue.replace(/\s+/g, ' ').trim())
-        setInputValue('')
+        console.log(formValues)
+        setFormValues(initialValues)
     }
 
     return (
         <form onSubmit={submitting} >
             <input
             type='text'
-            name='test'
-            placeHolder='Please type here'
-            value={inputValue}
+            name='name'
+            placeHolder='Please Type Name Here'
+            value={formValues.name}
             onChange={typing}
+            className='name'
             />
+            <button>Submit</button>
             <br/>
-            <label>
-                Submit Form
-                <br/>
-                <button>Submit Here Please</button>
-                <br/>
-                Definetly a Submit Form
-            </label>
+            <input
+            type='email'
+            name='email'
+            placeHolder='Please Type email Here'
+            value={formValues.email}
+            onChange={typing}
+            className='name'
+            />
+            <button>Submit</button>
+            <br/>
+            <label>Role
+          {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select value={formValues.role} name="role" onChange={typing}>
+            <option value="">-- Select a Role --</option>
+            <option value="Student">Student</option>
+            <option value="Instructor">Instructor</option>
+            <option value="Alumni">Alumni</option>
+          </select>
+        </label>
         </form>
     )
 }
