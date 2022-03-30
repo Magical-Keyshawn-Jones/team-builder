f56d43f412a4028c79872a7fa3adf4c64a8c64c6
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-29 19:00:07 -0500

    added the basic form input

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hi I'm paul, from the Jimmy Neutron</p>
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function Form (props) {
+    function typing () {
+        
+    }
+
+    return (
+        <form>
+            <input type='text' name='test' value='Type something' />
+        </form>
+    )
+}
+
+export default Form
