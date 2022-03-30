32a691e30b7211be06dbdf67bc0203a9efe974c3
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-29 19:37:46 -0500

    fixed extra spaces, prevented default, reset values

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,36 @@
 import { useState } from 'react';
 
 function Form (props) {
-    function typing () {
-        
+    const [inputValue, setInputValue] = useState('')
+
+    function typing (event) {
+        setInputValue(event.target.value)
+    }
+
+    function submitting (event) {
+        event.preventDefault()
+        inputValue.replace(/\s+/g, ' ').trim()
+        console.log(inputValue.replace(/\s+/g, ' ').trim())
+        setInputValue('')
     }
 
     return (
-        <form>
-            <input type='text' name='test' value='Type something' />
+        <form onSubmit={submitting} >
+            <input
+            type='text'
+            name='test'
+            placeHolder='Please type here'
+            value={inputValue}
+            onChange={typing}
+            />
+            <br/>
+            <label>
+                Submit Form
+                <br/>
+                <button>Submit Here Please</button>
+                <br/>
+                Definetly a Submit Form
+            </label>
         </form>
     )
 }
